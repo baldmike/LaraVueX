97502496dc3ab982e494cd97a2ea5c9f8427abd5
Bald Mike <baldmike312@gmail.com>
2018-07-04 21:26:48 -0500

    navbar dummy pages X & Y

---
+++ resources/assets/js/components/PageY.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>This is PageX</h1>
+</template>
+
+<script>
+    export default {
+        name: "pagey"
+    }
+</script>
---
+++ resources/assets/js/components/Playground.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="playgroundClass">
+        <h1>Playground</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+    name: "playground",
+
+}
+
+</script>
